037da48f4d58a0deb32acc0fe8ae8032fb8d08bb
JumboJumpo <giamp.manca@gmail.com>
2021-07-07 11:20:07 +0200

    router

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Slide } from '../interface/slide.interface';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  slides: Slide[] = [
+    {
+      title: 'titolo1',
+      description: 'descrizione1',
+      button: 'bottone1',
+      img: ''
+  },    {
+      title: 'titolo2',
+      description: 'descrizione2',
+      button: 'bottone2',
+      img: ''
+  },    {
+      title: 'titolo3',
+      description: 'descrizione3',
+      button: 'bottone3',
+      img: ''
+  },
+  ]
+
+activate_1: boolean;
+activate_2: boolean;
+activate_3: boolean;
+
+  constructor() {
+   }
 
   ngOnInit(): void {
   }
