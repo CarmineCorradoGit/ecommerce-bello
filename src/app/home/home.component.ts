90d2bbe2eadabd29b5473d49eff2833424b27ee2
JumboJumpo <giamp.manca@gmail.com>
2021-07-07 12:23:16 +0200

    slidecomponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -28,6 +28,24 @@ export class HomeComponent implements OnInit, OnDestroy {
       button: 'bottone3',
       isActive: false,
       img: 'https://via.placeholder.com/1920x500'
+  }, {
+      title: 'titolo4',
+      description: 'descrizione4',
+      button: 'bottone4',
+      isActive: false,
+      img: 'https://via.placeholder.com/1920x500'
+  }, {
+      title: 'titolo5',
+      description: 'descrizione5',
+      button: 'bottone5',
+      isActive: false,
+      img: 'https://via.placeholder.com/1920x500'
+  }, {
+      title: 'titolo6',
+      description: 'descrizione6',
+      button: 'bottone6',
+      isActive: false,
+      img: 'https://via.placeholder.com/1920x500'
   },
   ]
 
@@ -38,9 +56,37 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() {
     this.interval = setInterval(()=>{
       this.slides[this.index].isActive = false;
-      this.index++
+      if(this.index === this.slides.length - 1){
+        this.index = 0;
+      } else {
+        this.index++
+      }
       this.slides[this.index].isActive = true; 
-    }, 2000)
+    }, 5000)
+   }
+
+   selectSlide(i: number){
+    this.slides[this.index].isActive = false;
+    this.index = i;
+    this.slides[this.index].isActive = true;
+   }
+
+   carouselSlide(s: string){
+    this.slides[this.index].isActive = false;
+     if(s === 'prev'){
+      if(this.index === 0){
+        this.index = this.slides.length - 1;
+      } else {
+        this.index--
+      }
+     }else{
+       if(this.index === this.slides.length-1){
+         this.index = 0;
+       } else {
+        this.index++
+       }
+     }
+     this.slides[this.index].isActive = true;
    }
 
   ngOnInit(): void {
