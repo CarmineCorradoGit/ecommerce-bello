9388aebdb83676e8fb0c93978827ce159b30bdbe
ahmedRi94 <80628171+ahmedRi94@users.noreply.github.com>
2021-07-08 13:57:57 +0200

    add item section

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -11,8 +11,10 @@ import { Product } from '../interface/product.interface';
 export class ItemComponent implements OnInit {
 
   id: number;
-
+  img: any = ['https://montagnolirino.it/wp-content/uploads/2015/12/immagine-non-disponibile.png', 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTUyR_ZYPZ0knI5HE_dDeIuW2-rJTKxrozu9x5rVt01UF-osKT7R2qjJeH7uhTNt6lZK3qmHa50ITk&usqp=CAc', 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQbRFhzCdMZ_LcjYwFD8FVdqj-TLgeOu8pxOEuHd2ztModxJnTHPFb2iw_-KLqFSJxpFza7sxOUNQ&usqp=CAc', 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRfBIb372lAjZgaYg2u3Ij6xQQP6zVzkvxWzsXion1libXrMGqi1nl_9oiNYOvMjvJlbyWaStU9hmA&usqp=CAc'];
   product: Product;
+  imgShow: string;
+  onClick: boolean = false;
 
   constructor(private appService: AppService, private route: ActivatedRoute) { }
 
@@ -21,12 +23,25 @@ export class ItemComponent implements OnInit {
       this.id = params['id'];
       this.appService.getProduct(this.id).subscribe((res) => {
         this.product = res;
+        this.product.img = this.img;
+        if (this.product.img.length > 1) {
+          this.imgShow = this.product.img[1];
+        }
       })
     });
   }
 
-  // GoToCart(){
-
-  // }
+  changeImg(img: string) {
+    this.imgShow = img;
+  }
+  like(event) {
+    if (!this.onClick) {
+      event.target.classList.add('checked-heart');
+      this.onClick = true;
+    } else {
+      event.target.classList.remove('checked-heart');
+      this.onClick = false;
+    }
+  }
 
 }
